fe5928622c08607b418f58525566481e4eb03e02
Diktator <dychervony@gmail.com>
2023-10-31 21:31:05 +0200

    Добавил цветовую палитру

---
+++ ColorGenerator-MVVM/ColorGenerator/Models.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Threading.Tasks;
+
+namespace ColorGenerator {
+    class ColorModel {
+        public string color_name { get; set; }
+        public ColorModel(string color) => color_name = color;
+    }
+}
