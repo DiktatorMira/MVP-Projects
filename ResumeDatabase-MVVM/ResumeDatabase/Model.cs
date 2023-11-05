067184d39b698327a68e7a52ea1b56e2b49ba2e8
Diktator <dychervony@gmail.com>
2023-11-05 23:18:33 +0200

    Добавил базу резюме

---
+++ ResumeDatabase-MVVM/ResumeDatabase/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeDatabase {
+    [Serializable]
+    public class ResumeModel {
+        public string Fio, Age, FamilyStatus, Address, Email, IsInfo, IsLanguage, IsCommunicate;
+        public ResumeModel() {
+            Fio = Age = FamilyStatus = Address = Email = IsInfo = IsLanguage = IsCommunicate = string.Empty;
+        }
+    }
+}
